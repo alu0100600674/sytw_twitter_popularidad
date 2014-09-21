cec57a0b32bb69a78b4b90ea0bb1627b7e213ab4
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-09-21 23:22:31 +0100

    Modificado el fichero principal

--- twitter_popularidad.rb
+++ twitter_popularidad.rb
@@ -15,5 +15,12 @@ end
 post '/' do
 	@seguidores = []
 	client = my_twitter_client()
-	
-	
+	@nombre = params[:firstname] || ''
+	@numero = params[:n].to_i
+	@numero = 1 if @numero <= 0
+	if client.user? @nombre
+		seguidores = client.friend_ids("crguezl").attrs[:ids].take(10)
+		
+	end
+	erb :twitter
+end
