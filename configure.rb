527ed25a09d1aa3b22f8673ac5715e57e47d1721
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-09-21 23:16:50 +0100

    Actualizados los datos de configure.rb

--- configure.rb
+++ configure.rb
@@ -1,8 +1,8 @@
 def my_twitter_client
   Twitter::REST::Client.new do |config|
-    config.consumer_key        = '***********************'
-    config.consumer_secret     = '***********************'
-    config.access_token        = '***********************'
-    config.access_token_secret = '***********************'
+    config.consumer_key        = 'CW3EsjesFhPM5VmEVSkrlahHf'
+    config.consumer_secret     = 'RV80jyJb16PNYQeJ1tnfIk2aceWMifvHOtUz8thjV6toCLbGXi'
+    config.access_token        = '201502306-jWqi3OYxqfmBNy3OgMKrzXHR9SMd3zTzZ67CLtEm'
+    config.access_token_secret = 'pSYmER2788lXMb71nopyUQIDxzszj0OEDmIqgdO4POmSu'
   end
 end
