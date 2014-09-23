7a6d3460a25b6994c24a4e945a1f9cb51ce17bd4
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-09-23 22:45:16 +0100

    Mostrando el número de amigos introducido

--- twitter_popularidad.rb
+++ twitter_popularidad.rb
@@ -18,9 +18,24 @@ post '/' do
 	@name = params[:firstname] || ''
 	@number = params[:n].to_i
 	@number = 1 if @number <= 0
+	@number = 10 if @number > 10
 	if client.user? @name
-		seguidores = client.friend_ids("crguezl").attrs[:ids].take(10)
+# 		seguidores = client.friend_ids("jonay_bta").attrs[:ids].take(10)
+# 		seguidores = client.friends(@name,{})
+# 		@todo_tweet =(@todo_tweet != '') ? seguidores.map{ |i| i.name} : ''
+# 		@todo_tweet = ultimos_t.map { |i| i.name }
 		
+		
+# 		ultimos_t = client.user_timeline(@name,{:count=>@number.to_i})
+# 		@todo_tweet =(@todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''
+		
+# 		ultimos_t = client.friends(@name, {})
+		ultimos_t = client.friends(@name, {}).take(@number)
+# 		puts 'aaaaa'
+# 		puts ultimos_t
+		@todo_tweet = ultimos_t.map { |i| i.name }
+# 		puts 'bbbbb'
+# 		puts @todo_tweet
 	end
 	erb :twitter
 end
