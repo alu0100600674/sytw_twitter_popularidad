51da3e998b7c7eabf1cb004cc724a562c666d1f2
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-09-30 23:21:19 +0100

    Tests añadidos

--- Rakefile
+++ Rakefile
@@ -3,6 +3,7 @@ task :default do
 	sh "ruby twitter_popularidad.rb"
 end
 
+desc "Ejecutar los tests"
 task :test do
 	sh "ruby test/test.rb"
 end
--- test/test.rb
+++ test/test.rb
@@ -11,5 +11,19 @@ end
 
 describe "Contenido de la web" do
 	
+	it "Carga de la web" do
+		get '/'
+		assert last_response.ok?
+	end
+	
+	it "Titulo de la web" do
+		get '/'
+		assert last_response.head.include?("Amigos más populares de un usuario")
+	end
+	
+	it "Titulo de la web mostrado" do
+		get '/'
+		assert last_response.body.include?("Amigos más populares de un usuario")
+	end
 	
 end
